e1ed6bf0233815ba43621e7bb65d00be3eb3caf2
saeedkokash <saeedkokash@gmail.com>
2022-08-02 22:14:52 +0300

    the server is now running correctly according to code review

--- server.js
+++ server.js
@@ -5,29 +5,59 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
+const port = process.env.PORT || 4000;
 
+//just to make sure that the server works in home
+app.get('/', (req, res) => {
+  res.send('Hello this is first app.get now');
+});
 
+// declaring json data from location
 const weatherData = require('./data/weather.json');
 
-// console.log(weatherData);
+//using a statip json file for weather data
+// app.get('/weather', (req, res) => {
+//   res.send(weatherData);
+// });
 
-
-
-
-
-const port = process.env.PORT || 3001;
-
-app.get('/', (req, res) => {
-  res.send('Hello this is first app.get nowwww');
-});
 
 app.get('/weather', (req, res) => {
-  res.send(weatherData);
+  let searchQuery = req.query.searchQuery;
+
+  try {
+
+    const city = weatherData.find(value => value.city_name.toLowerCase() === searchQuery.toLowerCase());
+    const weatherArr = city.data.map(item => new Forecast(item));
+    res.status(200).send(weatherArr);
+
+  } catch (err) {
+
+    handleError(err, res);
+    // this could work also without function
+    // res.status(500).send('Something went wrong')
+  }
+
 });
+
+app.get('*', (req, res) => {res.status(404).send('page not found!')});
+
+
+//function to pass to api
+function handleError(error, res) {
+  res.status(500).send('Something went wrong');
+}
+
+
+//new object class
+class Forecast {
+  constructor(day) {
+    this.date = day.valid_date;
+    this.description = day.weather.description;
+  }
+}
 
 
 app.listen(port, () => {
-  console.log(`listening to port ${port}`);
+  console.log(`Server is starting at port ${port}`);
 });
