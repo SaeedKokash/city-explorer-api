9821f3c6c91bf5a30f88873a37c8411c92e5bd2c
saeedkokash <saeedkokash@gmail.com>
2022-08-03 23:37:45 +0300

    added lab09 requirements

--- server.js
+++ server.js
@@ -3,61 +3,89 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 4000;
 
 //just to make sure that the server works in home
-app.get('/', (req, res) => {
-  res.send('Hello this is first app.get now');
-});
-
-// declaring json data from location
-const weatherData = require('./data/weather.json');
-
-//using a statip json file for weather data
-// app.get('/weather', (req, res) => {
-//   res.send(weatherData);
+// app.get('/', (req, res) => {
+//   res.send('Hello this is first app.get now');
 // });
 
+// declaring json data from location
+// const weatherData = require('./data/weather.json');
 
-app.get('/weather', (req, res) => {
+// using a statip json file for weather data
+app.get('/weather', async (req, res) => {
   let searchQuery = req.query.searchQuery;
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+
+  const cityArr = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
 
   try {
 
-    const city = weatherData.find(value => value.city_name.toLowerCase() === searchQuery.toLowerCase());
-    const weatherArr = city.data.map(item => new Forecast(item));
-    res.status(200).send(weatherArr);
+    const cityData = cityArr.data.data.map(item => new Forecast(item));
+    res.status(200).send(cityData);
 
-  } catch (err) {
+  } catch (error) {
 
-    handleError(err, res);
-    // this could work also without function
-    // res.status(500).send('Something went wrong')
+    errorHandler(error, res);
   }
 
 });
 
-app.get('*', (req, res) => {res.status(404).send('page not found!')});
+app.get('/movies', async (req, res) => {
+  const searchQuery = req.query.searchQuery;
+
+  const movieArr = await axios.get(`http://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
+
+  try {
+
+    const movieData = movieArr.data.results.map(item => new Movie(item));
+    res.status(200).send(movieData);
+
+  } catch (error) {
+    errorHandler(error, res);
+  }
+});
 
 
-//function to pass to api
-function handleError(error, res) {
-  res.status(500).send('Something went wrong');
+//for anything that doesnt exist in code
+app.get('*', (req, res) => { res.status(404).send('Page not found!') });
+
+
+//function to send an error if found it will be deployed in the catch 
+function errorHandler(error, res) {
+  res.status(500).send({ error: 'Something went wrong' });
 }
+
 
 
 //new object class
 class Forecast {
   constructor(day) {
-    this.date = day.valid_date;
+    this.date = day.datetime;
     this.description = day.weather.description;
   }
 }
 
 
+class Movie {
+  constructor(movie) {
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = movie.poster_path;
+    this.popularity = movie.popularity;
+    this.released_on = movie.release_date;
+  }
+}
+
 app.listen(port, () => {
   console.log(`Server is starting at port ${port}`);
 });
+
